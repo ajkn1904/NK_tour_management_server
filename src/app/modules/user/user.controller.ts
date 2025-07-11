d162e70142a40f6c3a33ce8cbf2b2863887c7a9f
ajkn1904 <ajknishat@gmail.com>
2025-07-12 04:29:51 +0600

    sendResponse function added & API updated

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,28 +1,43 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */ 
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { userServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
 
 const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const user = await userServices.createUser(req.body);
 
-    res.status(StatusCodes.CREATED).json({
-      message: "User created successfully",
-      user,
-    });
+    // res.status(StatusCodes.CREATED).json({
+    //   message: "User created successfully",
+    //   user,
+    // });
+
+    sendResponse(res, {
+        statusCode: StatusCodes.CREATED,
+        success: true,
+        message: "User created successfully",
+        data: user,
+    })
 
 });
 
 
 
 const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-const users = await userServices.getAllUsers();
-    res.status(StatusCodes.OK).json({
+const result = await userServices.getAllUsers();
+    // res.status(StatusCodes.OK).json({
+    //     message: "All users retrieved successfully",
+    //     data: users,
+    // });
+
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
         message: "All users retrieved successfully",
-        data: users,
-    });
+        data: result.users,
+        meta: result.meta,
+    })
 
 })
 
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -3,21 +3,27 @@ import { User } from "./user.model";
 
 const createUser = async (payload: Partial<IUser>) => {
   const { name, email } = payload;
-
   const user = await User.create({ name, email });
-
   return user;
 };
 
 
 
 const getAllUsers = async () => {
-    const users = await User.find({});
-    return users;
+  const users = await User.find({});
+  const totalUsers = await User.countDocuments();
+
+  return {
+    users,
+    meta: {
+      total: totalUsers,
+    },
+  };
 };
+
 
 
 export const userServices = {
   createUser,
-  getAllUsers
+  getAllUsers,
 };
