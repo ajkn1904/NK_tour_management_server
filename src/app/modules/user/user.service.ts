6e9975624f4e7b8f573e8a05e6f918c15cd39416
ajkn1904 <ajknishat@gmail.com>
2025-07-12 23:13:44 +0600

    cradential updated & password hashing done

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,13 +1,32 @@
-import { IUser } from "./user.interface";
+import { StatusCodes } from "http-status-codes";
+import AppError from "../../errorHelper/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import bcryptjs from "bcryptjs";
 
 const createUser = async (payload: Partial<IUser>) => {
-  const { name, email } = payload;
-  const user = await User.create({ name, email });
+  const { email, password, ...rest } = payload;
+
+  const isUserExists = await User.findOne({ email });
+  if (isUserExists) {
+    throw new AppError(StatusCodes.BAD_REQUEST, "User already exists");
+  }
+
+  const authsProvider: IAuthProvider = {
+    provider: "credentials",
+    providerId: email as string,
+  };
+
+  const hashedPassword = await bcryptjs.hash(password as string, 10);
+
+  const user = await User.create({
+    email,
+    password: hashedPassword,
+    auths: [authsProvider],
+    ...rest,
+  });
   return user;
 };
-
-
 
 const getAllUsers = async () => {
   const users = await User.find({});
