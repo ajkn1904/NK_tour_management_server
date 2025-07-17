618fdb7e449badfbb87df430d744871c198be071
ajkn1904 <ajknishat@gmail.com>
2025-07-17 23:06:17 +0600

    interfaces & models for Tour & Division added

---
+++ src/app/modules/division/division.interface.ts
@@ -0,0 +1,13 @@
+export interface IDivision {
+    name: string;
+    slug: string;
+    thumbnail?: string;
+    description?: string
+}
+
+
+/**
+ * example:---------
+ * division name = Chattogram Division
+ * slug = chattogram-division
+*/
