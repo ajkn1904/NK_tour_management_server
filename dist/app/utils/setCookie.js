12a0131eff8a25c7436e0ecc86ba750e0ab73da1
ajkn1904 <ajknishat@gmail.com>
2025-08-18 01:06:57 +0600

    updated

--- dist/app/utils/setCookie.js
+++ dist/app/utils/setCookie.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.setAuthCookie = void 0;
-const env_1 = require("../config/env");
 const setAuthCookie = (res, tokenInfo) => {
     if (tokenInfo.accessToken) {
         res.cookie("accessToken", tokenInfo.accessToken, {
             httpOnly: true,
             //secure: false
-            secure: env_1.envVars.NODE_ENVIRONMENT === "production",
+            //secure: envVars.NODE_ENVIRONMENT === "production",
+            secure: true,
             sameSite: "none"
         });
     }
@@ -15,7 +15,8 @@ const setAuthCookie = (res, tokenInfo) => {
         res.cookie("refreshToken", tokenInfo.refreshToken, {
             httpOnly: true,
             //secure: false
-            secure: env_1.envVars.NODE_ENVIRONMENT === "production",
+            //secure: envVars.NODE_ENVIRONMENT === "production",
+            secure: true,
             sameSite: "none"
         });
     }
